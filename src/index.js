6e2842e14f5aebe22f9c3fefe98ccbea4788c49e
giacomosx <giacomo.bartoli@me.com>
2025-02-26 17:54:07 +0100

    change style

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap';
+document.head.appendChild(link);
+
+
 (function () {
     function createArtPayButton() {
         const targetSelector = "[data-artpay-button]";
@@ -12,13 +18,25 @@
         button.className = "artpay-button";
         button.innerHTML = `Paga con ArtPay`;
 
-        button.style.backgroundColor = "#4CAF50";
+        button.style.backgroundColor = '#241C3A'
         button.style.color = "white";
         button.style.border = "none";
-        button.style.padding = "15px 32px";
+        button.style.padding = "8px 24px";
         button.style.textAlign = "center";
-        button.style.fontSize = "16px";
+        button.style.fontSize = "18px";
         button.style.cursor = "pointer";
+        button.style.width = "100%";
+        button.style.borderRadius = "16px";
+        button.style.fontFamily = "Inter";
+        button.style.transition = "background-color 0.3s ease";
+
+        button.addEventListener('mouseover', function () {
+            button.style.backgroundColor = '#30264E';
+        })
+
+        button.addEventListener('mouseout', function () {
+            button.style.backgroundColor = '#241C3A';
+        })
 
         button.addEventListener("click", function () {
             const event = new CustomEvent("artpay:click", {
